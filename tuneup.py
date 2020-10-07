ec76cfe24780938822ec8c42062dd1f8c1dbc0a2
Meagan Ramey <meagan.ramey@yahoo.com>
2020-10-06 20:49:26 -0400

    Running into a NameError in my timeit function. Not sure where it's getting confused. Ran debugger to no avail. Will reach out for help in the morning.

--- tuneup.py
+++ tuneup.py
@@ -5,11 +5,12 @@
 Use the timeit and cProfile libraries to find bad code.
 """
 
-__author__ = "???"
+__author__ = "Meagan Ramey"
 
-import cProfile
-import pstats
-import functools
+# import cProfile
+# import pstats
+# import functools
+import timeit
 
 
 def profile(func):
@@ -50,6 +51,8 @@ def find_duplicate_movies(src):
 #
 # Students: write a better version of find_duplicate_movies
 #
+
+
 def optimized_find_duplicate_movies(src):
     # Your code here
     return
@@ -58,15 +61,16 @@ def optimized_find_duplicate_movies(src):
 def timeit_helper(func_name, func_param):
     """Part A: Obtain some profiling measurements using timeit"""
     assert isinstance(func_name, str)
-    # stmt = ???
-    # setup = ???
-    # t = ???
-    # runs_per_repeat = 3
-    # num_repeats = 5
-    # result = t.repeat(repeat=num_repeats, number=runs_per_repeat)
-    # time_cost = ???
-    # print(f"func={func_name}  num_repeats={num_repeats} runs_per_repeat={runs_per_repeat} time_cost={time_cost:.3f} sec")
-    # return t
+    stmt = f'{func_name}({func_param})'
+    setup = f'from tuneup import {func_name}'
+    t = timeit.Timer(stmt, setup)
+    runs_per_repeat = 3
+    num_repeats = 5
+    result = t.repeat(repeat=num_repeats, number=runs_per_repeat)
+    time_cost = result / num_repeats
+    print(f"""func={func_name}  num_repeats={num_repeats}
+            runs_per_repeat={runs_per_repeat} time_cost={time_cost:.3f} sec""")
+    return t
 
 
 def main():
@@ -90,9 +94,10 @@ def main():
 
     print("\n--- cProfile results, before optimization ---")
     profile(find_duplicate_movies)(filename)
-    
+
     print("\n--- cProfile results, after optimization ---")
     profile(optimized_find_duplicate_movies)(filename)
+
 
 if __name__ == '__main__':
     main()
